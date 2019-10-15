8653f81bd93d029da8db698a87abf69aaeeab34b
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2019-10-15 18:32:25 -0400

    Improve slideshow performance, Add slides

--- src/scripts/slideshow-v1.js
+++ src/scripts/slideshow-v1.js
@@ -1,22 +1,36 @@
 var slideshow_2018_2019 = {
-	imgSources: ["images/completed-drivetrain.jpg", "images/drive-team.jpg", "images/field.jpg"],
+	imgElements: [
+		document.getElementById("completed-drivetrain-img"),
+		document.getElementById("drive-team-img"),
+		document.getElementById("field-img"),
+		document.getElementById("match-start-img"),
+		document.getElementById("practice-field-cargo-img"),
+		document.getElementById("practice-field-hatch-panel-img")
+	],
 	imgCaptions: [
 		"Our robot's completed drivetrain", 
 		"Drive team members (from left to right): Emma Kanning, Jake Mitstifer, Boston Meeker, Matthew Taylor",
-		"The competition field for last year's game, Destination: Deep Space"
+		"The competition field for last year's game, Destination: Deep Space",
+		"Our robot before a match",
+		"Our robot scoring with a cargo element",
+		"Our robot scoring with a hatch panel"
 	],
 	currentImgIndex: 0,
-	currentSlideImg: document.getElementById("2018-2019-current-slide-img"),
-	currentSlideCaption: document.getElementById("2018-2019-current-slide-caption"),
+	slideCaptionElement: document.getElementById("2018-2019-current-slide-caption"),
 	
 	setImg: function() {
-		this.currentSlideImg.src = this.imgSources[this.currentImgIndex];
-		this.currentSlideCaption.innerHTML = this.imgCaptions[this.currentImgIndex];
+		this.imgElements[this.currentImgIndex].classList.add("current-slide");
+		for (var i = 0; i < this.imgElements.length; i++) {
+			if (i !== this.currentImgIndex) {
+				this.imgElements[i].classList.remove("current-slide");
+			}
+		}
+		this.slideCaptionElement.innerHTML = this.imgCaptions[this.currentImgIndex];
 	},
 	
 	increaseSlide: function() {
 		this.currentImgIndex++;
-		if (this.currentImgIndex >= this.imgSources.length) {
+		if (this.currentImgIndex >= this.imgElements.length) {
 			this.currentImgIndex = 0;
 		}
 		this.setImg();
@@ -24,7 +38,7 @@ var slideshow_2018_2019 = {
 	decreaseSlide: function() {
 		this.currentImgIndex--;
 		if (this.currentImgIndex < 0) {
-			this.currentImgIndex = this.imgSources.length - 1;
+			this.currentImgIndex = this.imgElements.length - 1;
 		}
 		this.setImg();
 	}
