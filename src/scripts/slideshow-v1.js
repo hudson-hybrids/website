9d3dbdebb8e6b61c455e255614ad90dfe5a95289
Matthew Taylor <matthewalantaylor@outlook.com>
2020-02-11 17:02:13 -0500

    Add 2019-2020 slideshow

--- src/scripts/slideshow-v1.js
+++ src/scripts/slideshow-v1.js
@@ -44,3 +44,50 @@ var slideshow_2018_2019 = {
 	}
 };
 slideshow_2018_2019.setImg();
+
+var slideshow_2019_2020 = {
+	imgElements: [
+		document.getElementById("engineering-img"),
+		document.getElementById("engineering-group-img"),
+		document.getElementById("frank-boston-img"),
+		document.getElementById("matthew-arshiq-img"),
+		document.getElementById("programming-group-img"),
+		document.getElementById("window-room-potato-img")
+	],
+	imgCaptions: [
+		"A member of the engineering team works on the robot", 
+		"Our engineers plan the design of our robot",
+		"Two of our engineers prototype the intank, tank, and outtank",
+		"Two of our programmers code the robot's camera system",
+		"Our programmers code the shooter",
+		"Our team has a dicussion about robot design"
+	],
+	currentImgIndex: 0,
+	slideCaptionElement: document.getElementById("2019-2020-current-slide-caption"),
+	
+	setImg: function() {
+		this.imgElements[this.currentImgIndex].classList.add("current-slide");
+		for (var i = 0; i < this.imgElements.length; i++) {
+			if (i !== this.currentImgIndex) {
+				this.imgElements[i].classList.remove("current-slide");
+			}
+		}
+		this.slideCaptionElement.innerHTML = this.imgCaptions[this.currentImgIndex];
+	},
+	
+	increaseSlide: function() {
+		this.currentImgIndex++;
+		if (this.currentImgIndex >= this.imgElements.length) {
+			this.currentImgIndex = 0;
+		}
+		this.setImg();
+	},
+	decreaseSlide: function() {
+		this.currentImgIndex--;
+		if (this.currentImgIndex < 0) {
+			this.currentImgIndex = this.imgElements.length - 1;
+		}
+		this.setImg();
+	}
+};
+slideshow_2019_2020.setImg();
