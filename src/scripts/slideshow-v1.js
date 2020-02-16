6d08a964a47b2074bb86db1bd56c2b75cb6952cc
Matthew Taylor <47510999+MatthewATaylor@users.noreply.github.com>
2020-02-16 14:27:21 -0500

    Add sponsors (in tiers), Change business lead, Add MailChimp form (#26)
    * Change fixed position navbar to absolute position
    
    * Move header image HTML to main.js
    
    * Replace header image with picture of our robot
    
    * Add version to JS links
    
    * Rename main.js to refresh cache
    
    * Add versioning to CSS files
    
    * Add email link
    
    * Add 404 page
    
    * Change header image for all pages except homepage
    
    * Update file versioning
    
    * Add programming links, Add photo slideshow
    
    * Improve slideshow performance, Add slides
    
    * Replace 'branches' with 'subteams'
    
    * Compress images, Add blur effect to header images
    
    * Update subteam leaders
    
    * Add social media accounts
    
    * Add meta description for main page
    
    * Add social media links
    
    * Add programming subteam description
    
    * Add new donation levels
    
    * Complete addition of donation details
    
    * Reword donation tiers
    
    * Add donation tier clarification note
    
    * Add sponsors
    
    * Add donation tiers, change business lead name
    
    * Update media.html
    
    Adds MailChimp
    
    * Add signature tier, add NASA logo, Add bronze tier
    
    * Update media.html
    
    * Update media.html
    
    * Style donation tier presentation
    
    * Change business lead
    
    * Clean up MailChimp form HTML
    
    * Add 2019-2020 slideshow
    
    * Add more 2019-2020 images
    
    Co-authored-by: Howie7363 <57737306+Howie7363@users.noreply.github.com>
    
    Closes #18

--- src/scripts/slideshow-v1.js
+++ src/scripts/slideshow-v1.js
@@ -44,3 +44,62 @@ var slideshow_2018_2019 = {
 	}
 };
 slideshow_2018_2019.setImg();
+
+var slideshow_2019_2020 = {
+	imgElements: [
+		document.getElementById("engineering-img"),
+		document.getElementById("engineering-group-img"),
+		document.getElementById("frank-boston-img"),
+		document.getElementById("matthew-arshiq-img"),
+		document.getElementById("programming-group-img"),
+		document.getElementById("window-room-potato-img"),
+		document.getElementById("day1-discussion-1-img"),
+		document.getElementById("drive-train-img"),
+		document.getElementById("field-element-img"),
+		document.getElementById("business-brainstorm-img"),
+		document.getElementById("vision-img"),
+		document.getElementById("wiring-img")
+	],
+	imgCaptions: [
+		"Begining wiring of the robot's electronics", 
+		"Our engineers plan the design of our robot",
+		"Two of our engineers prototype the robot's tank",
+		"Two of our programmers code the robot's camera system",
+		"Our programmers code the shooter",
+		"Our team has a dicussion about robot design",
+		"One group of students discusses ideas just after the game was revealed",
+		"Members of the engineering team work on the drivetrain of our new robot",
+		"Members of the engineering team create a replica of a field element",
+		"Members of the busiess team working together on a business plan",
+		"Vision processor lighting up the room",
+		"Programming the new electronics board"
+	],
+	currentImgIndex: 0,
+	slideCaptionElement: document.getElementById("2019-2020-current-slide-caption"),
+	
+	setImg: function() {
+		this.imgElements[this.currentImgIndex].classList.add("current-slide");
+		for (var i = 0; i < this.imgElements.length; i++) {
+			if (i !== this.currentImgIndex) {
+				this.imgElements[i].classList.remove("current-slide");
+			}
+		}
+		this.slideCaptionElement.innerHTML = this.imgCaptions[this.currentImgIndex];
+	},
+	
+	increaseSlide: function() {
+		this.currentImgIndex++;
+		if (this.currentImgIndex >= this.imgElements.length) {
+			this.currentImgIndex = 0;
+		}
+		this.setImg();
+	},
+	decreaseSlide: function() {
+		this.currentImgIndex--;
+		if (this.currentImgIndex < 0) {
+			this.currentImgIndex = this.imgElements.length - 1;
+		}
+		this.setImg();
+	}
+};
+slideshow_2019_2020.setImg();
